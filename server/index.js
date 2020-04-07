588a403a24c4c4c7511af36dfd4f9f0c9a622ead
SaloniSS <sxs170015@utdallas.edu>
2020-04-07 12:36:57 -0500

    Started work on serevr

---
+++ server/index.js
@@ -0,0 +1,19 @@
+//Source: https://www.youtube.com/watch?v=KyWaXA_NvT0&t=7s
+
+const express = require('express');
+const dotenv = require('dotenv');
+const colors = require('colors');
+const morgan = require('morgan');
+
+dotenv.config({ path:'./config/config.env' });
+
+const app = express();
+
+app.use((req, res) => res.send('Hello World'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+    PORT, 
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
+);

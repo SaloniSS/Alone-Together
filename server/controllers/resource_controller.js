9a80f676f1b7a2fea940d54f0da5a39434af8fe6
SaloniSS <sxs170015@utdallas.edu>
2020-04-10 00:47:49 -0500

    GET api

--- server/controllers/resource_controller.js
+++ server/controllers/resource_controller.js
@@ -1,13 +1,27 @@
+const Resource = require ('../models/Resource');
+
 //@desc     Get all resources
 //@route    GET /api/v1/resources
 //@access   Public
-exports.getResources = (req, res, next) => {
-    res.send('GET resources');
+exports.getResources = async (req, res, next) => {
+    try {
+        const resources = await Resource.find();
+        return res.status(200).json({
+            success: true,
+            count: resources.length,
+            data: resources
+        })
+    } catch (error) {
+        res.send(500).json({
+            success: false,
+            error: 'Server Error'
+        });
+    }
 }
 
 //@desc     Add all resources
 //@route    POST /api/v1/resources
 //@access   Public
-exports.addResources = (req, res, next) => {
+exports.addResources = async (req, res, next) => {
     res.send('POST resources');
 }

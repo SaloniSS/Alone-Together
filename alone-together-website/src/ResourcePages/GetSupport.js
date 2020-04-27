43d6285d3b17b0ec8209b07ee755c9c2daf94a1c
SaloniSS <sxs170015@utdallas.edu>
2020-04-27 14:33:26 -0500

    Change to state

--- alone-together-website/src/ResourcePages/GetSupport.js
+++ alone-together-website/src/ResourcePages/GetSupport.js
@@ -1,58 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import { Container, Row, Col, Card, CardBody, CardTitle, CardText} from 'reactstrap';
 
 import './ResourcePages.css';
 import db from '../firebase';
 
-class GetSupport extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        resources: []
-      };
-    }
+const GetSupport = () => {
+
+    const [resources, setResources] = useState([]);
   
-    componentDidMount() {
+    useEffect(() => {
         db.collection("To Get Support").get().then(querySnapshot => {
           const data = querySnapshot.docs.map(doc => doc.data());
           console.log(data);
-          this.setState({ resources: data });
+          setResources(data);
         });
-    }
+    });
 
-    render() {
-        const { resources } = this.state;
-        return(
-            <div>
-                <div className="header">
-                    <h1>💜 Get Supported 💜</h1>
-                    <h2>Online Resources</h2>
-                </div>
-                <div className="header-description">
-                    <p>
-                        Share free resources on how to cope with anxiety and stress from the coronavirus. 
-                        Articles, guidebooks, or resources that other brands / organizations have created. 
-                    </p>
-                </div>
-                <div className="resource">
-                    <Col className="card-padding">
-                        {resources.map (resource => (
-                            <a href={resource.link} target="_blank">
-                                <Card className="portfolio-card">
-                                    <CardBody>
-                                        <CardTitle>{resource.name}</CardTitle>
-                                        <CardText><strong>Medium: </strong> {resource.medium} </CardText>
-                                        <CardText><strong>Description: </strong> {resource.description} </CardText>
-                                        <CardText><strong>Submitted By: </strong> {resource.submittedBy} </CardText>
-                                    </CardBody>
-                                </Card>
-                            </a>
-                        ))}
-                    </Col>
-                </div>
-            </div>       
-        );
-    }
+    return(
+        <div>
+            <div className="header">
+                <h1>💜 Get Supported 💜</h1>
+                <h2>Online Resources</h2>
+            </div>
+            <div className="header-description">
+                <p>
+                    Share free resources on how to cope with anxiety and stress from the coronavirus. 
+                    Articles, guidebooks, or resources that other brands / organizations have created. 
+                </p>
+            </div>
+            <div className="resource">
+                <Col className="card-padding">
+                    {resources.map (resource => (
+                        <a href={resource.link} target="_blank">
+                            <Card className="portfolio-card">
+                                <CardBody>
+                                    <CardTitle>{resource.name}</CardTitle>
+                                    <CardText><strong>Medium: </strong> {resource.medium} </CardText>
+                                    <CardText><strong>Description: </strong> {resource.description} </CardText>
+                                    <CardText><strong>Submitted By: </strong> {resource.submittedBy} </CardText>
+                                </CardBody>
+                            </Card>
+                        </a>
+                    ))}
+                </Col>
+            </div>
+        </div>       
+    );
 }
 
 export default GetSupport;
--- alone-together-website/src/ResourcePages/Relax.js
+++ alone-together-website/src/ResourcePages/Relax.js
@@ -1,59 +1,52 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Container, Row, Col, Card, CardBody, CardTitle, CardText} from 'reactstrap';
 import { Link } from 'react-router-dom';
 
 import './ResourcePages.css';
 import db from '../firebase';
 
-class Relax extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        resources: []
-      };
-    }
+const Relax = () => {
+
+    const [resources, setResources] = useState([]);
   
-    componentDidMount() {
+    useEffect(() => {
         db.collection("To Relax").get().then(querySnapshot => {
           const data = querySnapshot.docs.map(doc => doc.data());
           console.log(data);
-          this.setState({ resources: data });
+          setResources(data);
         });
-    }
+    });
 
-    render() {
-        const { resources } = this.state;
-        return(
-            <div>
-                <div className="header">
-                    <h1>📖 To Relax 📖</h1>
-                    <h2>The Fun Stuff!</h2>
-                </div>
-                <div className="header-description">
-                    <p>
-                        Share what you've been reading, watching, consuming that you think are *must do* for the community. 
-                        Sky's the limit!
-                    </p>
-                </div>
-                <div className="resource">
-                    <Col className="card-padding">
-                        {resources.map (resource => (
-                            <a href={resource.link} target="_blank">
-                                <Card className="portfolio-card">
-                                    <CardBody>
-                                        <CardTitle>{resource.name}</CardTitle>
-                                        <CardText><strong>Medium: </strong> {resource.medium} </CardText>
-                                        <CardText><strong>Description: </strong> {resource.description} </CardText>
-                                        <CardText><strong>Submitted By: </strong> {resource.submittedBy} </CardText>
-                                    </CardBody>
-                                </Card>
-                            </a>
-                        ))}
-                    </Col>
-                </div>
-            </div>       
-        );
-    }
+    return(
+        <div>
+            <div className="header">
+                <h1>📖 To Relax 📖</h1>
+                <h2>The Fun Stuff!</h2>
+            </div>
+            <div className="header-description">
+                <p>
+                    Share what you've been reading, watching, consuming that you think are *must do* for the community. 
+                    Sky's the limit!
+                </p>
+            </div>
+            <div className="resource">
+                <Col className="card-padding">
+                    {resources.map (resource => (
+                        <a href={resource.link} target="_blank">
+                            <Card className="portfolio-card">
+                                <CardBody>
+                                    <CardTitle>{resource.name}</CardTitle>
+                                    <CardText><strong>Medium: </strong> {resource.medium} </CardText>
+                                    <CardText><strong>Description: </strong> {resource.description} </CardText>
+                                    <CardText><strong>Submitted By: </strong> {resource.submittedBy} </CardText>
+                                </CardBody>
+                            </Card>
+                        </a>
+                    ))}
+                </Col>
+            </div>
+        </div>       
+    );
 }
 
 export default Relax;
